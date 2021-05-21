601e82d26c08d8f6e8d8837a6dc20670e4aa3639
bavshehata <bavshehata@gmail.com>
2021-05-21 21:06:28 +0200

    selecting products

--- demonode/pages/index.js
+++ demonode/pages/index.js
@@ -1,13 +1,29 @@
 import { Page } from "@shopify/polaris";
-
-const Index = () => (
-  <Page
-    title="Product Selector"
-    primaryAction={{
-      content: "Select products",
-      onAction: () => alert("Clicked"),
-    }}
-  />
-);
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import { useState } from "react";
+const Index = () => {
+  const [open, setOpen] = useState(false);
+  console.log(open);
+  const handleResources = (resources) => {
+    setOpen(false);
+    console.log(resources);
+  };
+  return (
+    <Page
+      title="Product Selector"
+      primaryAction={{
+        content: "Select products",
+        onAction: () => setOpen(true),
+      }}
+    >
+      <ResourcePicker
+        resourceType="Product"
+        open={open}
+        onCancel={() => setOpen(false)}
+        onSelection={(resources) => handleResources(resources)}
+      />
+    </Page>
+  );
+};
 
 export default Index;
