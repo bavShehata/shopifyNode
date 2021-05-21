769d20436fdbbbf29d941ffbe3149ec9088583e6
bavshehata <bavshehata@gmail.com>
2021-05-21 20:44:27 +0200

    simple UI

--- demonode/pages/index.js
+++ demonode/pages/index.js
@@ -1,5 +1,13 @@
 import { Page } from "@shopify/polaris";
 
-const Index = () => <Page />;
+const Index = () => (
+  <Page
+    title="Product Selector"
+    primaryAction={{
+      content: "Select products",
+      onAction: () => alert("Clicked"),
+    }}
+  />
+);
 
 export default Index;
